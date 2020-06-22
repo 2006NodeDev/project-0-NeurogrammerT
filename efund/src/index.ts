f7f1c4976c15adc248ea2dfc61e69e6b247b7446
NeurogrammerT <bbstarnique@aol.com>
2020-06-22 12:59:01 -0400

    initial setup

---
+++ efund/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.use('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(2006, () => {
+    console.log('Server Has Started');
+    
+})
