8a867f507c4d3777a66d5aa5481132f8a127f0c2
NeurogrammerT <bbstarnique@aol.com>
2020-06-26 10:15:23 -0400

    logging stuff

--- efund/src/index.ts
+++ efund/src/index.ts
@@ -1,6 +1,8 @@
-import express from 'express'
-import { userRouter, } from './routers/user-router'
+import express, { Request, Response} from 'express'
+import { userRouter, users, } from './routers/user-router'
 import { reimbursementRouter } from './routers/reimbursement-router'
+import { InvalidEntryError } from './errors/InvalidEntryError'
+import { AuthenticationError } from './errors/authenticationError'
 
 const app = express()
 
@@ -8,6 +10,44 @@ app.use(express.json())
 
 app.use('/users', userRouter)
 app.use('/reimbursements', reimbursementRouter)
+
+app.post('/login', (req:Request, res:Response)=>{
+    
+    let username = req.body.username
+    let password = req.body.password
+    
+    if(!username || !password){
+        
+        throw new InvalidEntryError()
+    } else {
+        let found = false
+        for(const user of users) {
+            if(user.username === username && user.password === password){
+                
+                req.session.user = user
+                
+                res.json(user)
+                found = true
+            }
+        }
+        if(!found){
+            throw new AuthenticationError()
+        }
+    }
+})
+
+app.use((err, req, res, next) => {
+
+    if (err.statusCode) {
+        
+        res.status(err.statusCode).send(err.message)
+    } else {
+       
+        console.log(err)
+        
+        res.status(500).send('Oops, Something went wrong')
+    }
+})
 
 app.listen(2006, () => {
     console.log('Server Has Started');
